b40233db1d73960fdecbefe635a9903fceb2e91b
Anonno <anonnodas97@gmail.com>
2024-11-27 00:42:01 +0600

    add social navigation

--- src/components/HomePage/IntroduceMySelf.tsx
+++ src/components/HomePage/IntroduceMySelf.tsx
@@ -1,6 +1,9 @@
 import profileImage from "../../assets/images/Anonno Das.jpg";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { FaDiscord, FaGithub, FaInstagram } from "react-icons/fa";
 import { IoEye } from "react-icons/io5";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FiMail } from "react-icons/fi";
 const IntroduceMySelf = () => {
   return (
     <div className="flex justify-between items-center mt-24 px-16">
@@ -30,6 +33,50 @@ const IntroduceMySelf = () => {
             </span>
             resume
           </button>
+        </div>
+        <div className="flex gap-5 mt-5 relative">
+          <a
+            href="https://github.com/mranonno"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub
+              size={24}
+              className="hover:-translate-y-2 transition-transform duration-300 text-gray-500 hover:text-fuchsia-500"
+            />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/mranonno1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn
+              size={24}
+              className="hover:-translate-y-2 transition-transform duration-300 text-gray-500 hover:text-fuchsia-500"
+            />
+          </a>
+          <a href="mailto:anonnodas97@gmail.com">
+            <FiMail
+              className="hover:-translate-y-2 transition-transform duration-300 text-gray-500 hover:text-fuchsia-500"
+              size={24}
+            />
+          </a>
+
+          <FaDiscord
+            size={24}
+            className="hover:-translate-y-2 transition-transform duration-300 text-gray-500 hover:text-fuchsia-500"
+          />
+          <a
+            href="https://www.instagram.com/mranonno1/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram
+              size={24}
+              className="hover:-translate-y-2 transition-transform duration-300 text-gray-500 hover:text-fuchsia-500"
+            />
+          </a>
         </div>
       </div>
       <div className="rotate-6 relative hover:scale-95 transition duration-1000 mr-24">
