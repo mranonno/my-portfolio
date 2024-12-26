610b359df0715fe57026c376dbb2222857c24cef
Anonno Das <anonnodas97@gmail.com>
2024-12-27 02:09:05 +0600

    footer design

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -4,10 +4,12 @@ import SocialLink from "./SocialLink";
 const Footer = () => {
   return (
     <div className="mt-12">
-      <div className="border mb-4" />
-      <div className="flex justify-between">
-        <div className="flex-[0.5]">
-          <div></div>
+      <div className="border mb-8" />
+      <div className="flex items-center pb-4 gap-24">
+        <div className="flex-[0.7]">
+          <div>
+            <h3 className="text-3xl text-fuchsia-800 py-4">[LOGO]</h3>
+          </div>
           <p className="text-justify">
             Self-driven focused, and dedicated Software Develop with 1+ years of
             experience, including 6+ month of industrial experience. I am
@@ -21,22 +23,22 @@ const Footer = () => {
         </div>
         <div className="flex gap-24 pr-16">
           <div className="flex flex-col gap-5">
-            <h3>Explore</h3>
+            <h3 className="text-2xl font-medium text-gray-900">Explore</h3>
             <Link to={"/projects"}>Project</Link>
             <Link to={"/articles"}>Articles</Link>
-            <Link>Snippets</Link>
-            <Link>Workspace</Link>
+            <Link to={""}>Snippets</Link>
+            <Link to={""}>Workspace</Link>
           </div>
           <div className="flex flex-col gap-5">
-            <h3>About</h3>
+            <h3 className="text-2xl font-medium text-gray-900">About</h3>
             <Link to={"/about"}>About</Link>
-            <Link>Today</Link>
-            <Link>Resume</Link>
+            <Link to={""}>Today</Link>
+            <Link to={""}>Resume</Link>
           </div>
         </div>
       </div>
-      <div className="border my-4" />
-      <div className="text-center">
+      <div className="border my-8" />
+      <div className="text-center space-y-2 mb-8">
         <h3>Designed & Developed by Anonno</h3>
         <p>Copyright © 2023 - All right reserved by Anonno</p>
       </div>
