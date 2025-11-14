50956871fdd0c3778aa687d289f8d6cba2a68cb1
Anonno <anonnodas97@gmail.com>
2025-11-15 01:16:56 +0600

    working on theme

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -6,6 +6,7 @@ import logoImg from "../../assets/logo/logo_anonno.png";
 const Navbar = () => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
   console.log(isChecked);
+
   const getNavLinkClass = (isActive: boolean) => {
     return isActive
       ? "text-fuchsia-700 font-poppins text-lg font-medium"
@@ -63,42 +64,6 @@ const Navbar = () => {
             checked={isChecked}
             onCheckedChange={() => setIsChecked(!isChecked)}
           />
-          {/* <label className="grid cursor-pointer place-items-center">
-            <input
-              type="checkbox"
-              value="synthwave"
-              className="toggle theme-controller bg-base-content col-span-2 col-start-1 row-start-1"
-            />
-            <svg
-              className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <circle cx="12" cy="12" r="5" />
-              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-            </svg>
-            <svg
-              className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-            </svg>
-          </label> */}
         </div>
       </div>
     </div>
